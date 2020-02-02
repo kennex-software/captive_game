09d4fc213f619da47fee70266ed059c7adf51377
kennex09 <53454116+kennex09@users.noreply.github.com>
2020-02-02 12:12:39 -0500

    Got the falling numbers channel working

--- tv_channels.py
+++ tv_channels.py
@@ -66,6 +66,26 @@ def whitespace(surface, x, y, h, w):
         y += pixel_size
         x = start
 
+def falling_numbers(gs, surface, x, y, h, w):
+    gs.current_tv_screen_color = gs.white
+
+    range_num = 11
+    factor = h / (range_num + 1.75)
+    start = x
+
+    number_grid = [range(1, range_num) for n in range(14)]
+    colors = [(gs.white), (gs.black), (gs.red), (gs.blue)]
+
+    for row in number_grid:
+        for col in row:
+            color = random.randint(0, 2)
+            text_image = gs.verdana16.render(str(col), True, colors[color])
+            surface.blit(text_image, (x, y))
+
+            x += text_image.get_width() + factor
+        y += text_image.get_height()
+        x = start
+
 def tv_channels(gs, screen):
     """
     Function to hold and display all information that could be found on the TV.
@@ -80,14 +100,14 @@ def tv_channels(gs, screen):
     """
     tv_rect = pygame.Rect(195, 140, 470, 296)
     partial_tv_rect = pygame.Rect(945, 140, 470, 296)
-    whitespace_y = 140
-    whitespace_w = 296
-    whitespace_h = 470
+    tv_y = 140
+    tv_w = 296
+    tv_h = 470
 
     if gs.current_room_view == 1:
-        whitespace_x = 195
+        tv_x = 195
     else:
-        whitespace_x = 945
+        tv_x = 945
 
 
     if gs.current_channel == str(0):  # Whitespace todo need perlin noise?
@@ -103,7 +123,7 @@ def tv_channels(gs, screen):
     elif gs.current_channel == str(2):  # Flash on screen
         print(gs.current_channel)
     elif gs.current_channel == str(3):  # Default channel??
-        print(gs.current_channel)
+        pass
     elif gs.current_channel == str(4):  # Camera 1
         print(gs.current_channel)
     elif gs.current_channel == str(5):  # Camera 2
@@ -111,9 +131,9 @@ def tv_channels(gs, screen):
     elif gs.current_channel == str(6):  # Camera 3
         print(gs.current_channel)
     elif gs.current_channel == str(7):  # Whitespace
-        print(gs.current_channel)
+        falling_numbers(gs, screen, tv_x, tv_y, tv_h, tv_w)
     elif gs.current_channel == str(8):  # Whitespace
-        whitespace(screen, whitespace_x, whitespace_y, whitespace_h, whitespace_w)
+        whitespace(screen, tv_x, tv_y, tv_h, tv_w)
     elif gs.current_channel == str(9):  # Black Screen
         print(gs.current_channel)
     elif gs.current_channel == str(gs.channel_code):
@@ -126,6 +146,8 @@ def tv_channels(gs, screen):
         print(gs.current_channel)
     elif gs.current_channel == str(456): # todo easter egg channel for fun
         print(gs.current_channel)
+    elif gs.current_channel == str(181161693114): # This spells "RAPPICAN" if you put 1-26 next to the alphabet
+        print("PAUL'S CHANNEL")
     else:  # Whitespace todo perlin noise
-        whitespace(screen, whitespace_x, whitespace_y, whitespace_h, whitespace_w)
+        whitespace(screen, tv_x, tv_y, tv_h, tv_w)
 
